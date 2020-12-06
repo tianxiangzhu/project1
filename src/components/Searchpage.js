8b7af9eb91401a649a6de1805c5d9962cacc99cf
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-12-06 11:49:49 -0700

    Create Searchpage.js

---
+++ src/components/Searchpage.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+    Link,
+    Redirect
+  } from "react-router-dom";
+import data from "../database/data";
+  class Searchpage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {searchtext:"",businesses:[], showbusiness: false, city:"", state: ""}
+        
+    }
+    onSubmit = (event) => {
+        this.setState({showbusiness: true}) 
+        event.preventDefault();
+    }
+    livesearch = (event) => {
+        const text = event.target.value;
+        const businesses = data.findbusinesses(text,this.state.city,this.state.state)
+        this.setState({ businesses: businesses,searchtext: text});
+    }
+    citysearch = (event) => {
+        const text = event.target.value;
+        this.setState({ city: text,});
+    }
+    statesearch =  (event) => {
+        const text = event.target.value;
+        this.setState({ state: text,});
+    }
+
+    delete = (event) => {
+        data.deletebusiness(event.target.getAttribute("business"))
+        this.forceUpdate()
+    }
+    
+    render(){
+        const buslist= this.state.businesses.filter(b => b.active)
+        const busdiv= buslist.map(
+            (b) => <div>
+                {b.name},{b.address}, {b.city}, {b.state} {b.zip}, {b.phone}   (
+                <Link
+                    to={{
+                    pathname: "/update",
+                    state: { id: b.id }
+                }}
+             >Update</Link>
+             <a  onClick={this.delete} business= {b.id} >Delete</a>)
+            </div>
+        )
+        
+        
+        if (this.state.showbusiness) {
+            const bs = buslist.map(b => b.id)
+            let to= {pathname: '/business', state: { bs: bs } };
+            return (
+                <Redirect to={to} />
+            );
+
+        }
+
+        
+        return(
+        <div>
+                <form onSubmit={this.onSubmit}>
+                    <p>Where is your location?</p>
+                    <input
+                        type="text"
+                        value={this.state.searchcity}
+                        placeholder = "City"
+                        onChange={this.citysearch}
+                        required
+                    ></input>
+                    <br></br>
+                    <input
+                        type="text"
+                        value={this.state.searchstate}
+                        placeholder = "State"
+                        onChange={this.statesearch}
+                        required
+                    ></input>
+                    
+                    <br/>
+                    <p>Looking for something?</p>
+                    <input
+                        type="text"
+                        value={this.state.searchtext}
+                        placeholder = "Keywords for searching"
+                        onChange={this.livesearch}
+                        required
+                    ></input><br></br><br></br>
+                    <button type="submit">Go see reviews</button>
+                </form>
+                <br></br>
+                <h4>Here are what you might be looking for: </h4>
+                {busdiv}
+                <hr></hr>
+
+        </div>
+        )
+    }
+}
+export default Searchpage;
